8b9eed132383df3411cbf7ba0420edcccca44c2c
jiangjiesheng <dev@jiangjiesheng.com>
2018-07-19 17:25:19 +0800

    修改版权和访问统计

---
+++ tongji.js
@@ -0,0 +1,38 @@
+var _hmt = _hmt || [];
+(function() {
+	var hm = document.createElement("script");
+	hm.src = "//hm.baidu.com/hm.js?0a07851c5312e73abde177253ad4e1b0";
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(hm, s);
+})();
+
+<!-- cnzz统计 -->
+var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1260640953'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D1260640953%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
+
+/*51统计*/
+
+/* (function() {
+	var hm51 = document.createElement("script");
+	hm51.src = "http://js.users.51.la/19008474.js";
+	var s51 = document.getElementsByTagName("script")[0];
+	s51.parentNode.insertBefore(hm51, s51);
+})();*/
+
+/*document.write ('<a href="https://www.51.la/?19008474" target="_blank" title="51.La &#x7F51;&#x7AD9;&#x6D41;&#x91CF;&#x7EDF;&#x8BA1;&#x7CFB;&#x7EDF;"><img alt="51.La &#x7F51;&#x7AD9;&#x6D41;&#x91CF;&#x7EDF;&#x8BA1;&#x7CFB;&#x7EDF;" src="//icon.users.51.la/icon_8.gif" style="border:none" /></a>\n');
+var a8474tf="51la";var a8474pu="";var a8474pf="51la";var a8474su=window.location;var a8474sf=document.referrer;var a8474of="";var a8474op="";var a8474ops=1;var a8474ot=1;var a8474d=new Date();var a8474color="";if (navigator.appName=="Netscape"){a8474color=screen.pixelDepth;} else {a8474color=screen.colorDepth;}
+try{a8474tf=top.document.referrer;}catch(e){}
+try{a8474pu =window.parent.location;}catch(e){}
+try{a8474pf=window.parent.document.referrer;}catch(e){}
+try{a8474ops=document.cookie.match(new RegExp("(^| )a8474_pages=([^;]*)(;|$)"));a8474ops=(a8474ops==null)?1: (parseInt(unescape((a8474ops)[2]))+1);var a8474oe =new Date();a8474oe.setTime(a8474oe.getTime()+60*60*1000);document.cookie="a8474_pages="+a8474ops+ ";path=/;expires="+a8474oe.toGMTString();a8474ot=document.cookie.match(new RegExp("(^| )a8474_times=([^;]*)(;|$)"));if(a8474ot==null){a8474ot=1;}else{a8474ot=parseInt(unescape((a8474ot)[2])); a8474ot=(a8474ops==1)?(a8474ot+1):(a8474ot);}a8474oe.setTime(a8474oe.getTime()+365*24*60*60*1000);document.cookie="a8474_times="+a8474ot+";path=/;expires="+a8474oe.toGMTString();}catch(e){}
+try{if(document.cookie==""){a8474ops=-1;a8474ot=-1;}}catch(e){}
+a8474of=a8474sf;if(a8474pf!=="51la"){a8474of=a8474pf;}if(a8474tf!=="51la"){a8474of=a8474tf;}a8474op=a8474pu;try{lainframe}catch(e){a8474op=a8474su;}
+a8474src='//web.users.51.la/go.asp?svid=9&id=19008474&tpages='+a8474ops+'&ttimes='+a8474ot+'&tzone='+(0-a8474d.getTimezoneOffset()/60)+'&tcolor='+a8474color+'&sSize='+screen.width+','+screen.height+'&referrer='+escape(a8474of)+'&vpage='+escape(a8474op)+'&vvtime='+a8474d.getTime();
+setTimeout('a8474img = new Image;a8474img.src=a8474src;',0);*/
+
+/* 调用(具体路径再调整)  
+ * <div style="position:absolute; bottom:10px;visibility: hidden">
+	<script src="../../tongji.js" type="text/javascript" charset="utf-8"></script>
+</div>*/
+
+//<script type="text/javascript" color="0,209,178" opacity='0.8' zIndex="-2" count="50" src="//cdn.bootcss.com/canvas-nest.js/1.0.1/canvas-nest.min.js"></script>
